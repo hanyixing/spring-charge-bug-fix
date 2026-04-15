cd00d84bac308c950a976a356758f7a9dcf87db0
hyx <1120546503@qq.com>
2026-04-15 12:11:53 +0800

    fix: 修复时段索引计算边界错误

--- src/main/java/com/example/demo/service/ChargingRewardService.java
+++ src/main/java/com/example/demo/service/ChargingRewardService.java
@@ -139,11 +139,6 @@ public class ChargingRewardService {
         
         int pointIndex = (hour * 60 + minute) / MINUTES_PER_POINT;
         
-        if (minute % MINUTES_PER_POINT == 0 && minute > 0) {
-            pointIndex = pointIndex - 1;
-        }
-        
-        // 确保索引在有效范围内 [0, POINTS_PER_DAY - 1]
         pointIndex = pointIndex % POINTS_PER_DAY;
         if (pointIndex < 0) {
             pointIndex = pointIndex + POINTS_PER_DAY;
